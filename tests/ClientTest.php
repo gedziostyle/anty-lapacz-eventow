2b4fc8cbd79e13eb3f3c0b311ed322bceee82e33
jarekkowol <jarekkowol@gmail.com>
2018-03-27 15:25:22 +0200

    update

---
+++ tests/ClientTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AntyLapaczEventow;
+
+use PHPUnit\Framework\TestCase;
+
+class ClientTest extends TestCase
+{
+    public function testExecute()
+    {
+        $this->assertTrue(true);
+    }
+}
